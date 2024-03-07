4f6ef11033cf8b542c0c013a58306ea911fbcefc
Quasar <trottinger25@pascack.org>
2024-03-07 08:14:55 -0500

    Next Page function JS

--- script/script.js
+++ script/script.js
@@ -7,14 +7,21 @@ initializePages();
 function initializePages() {
     pages = document.getElementsByClassName('page');
     // pages = document.querySelectorAll('.page');
-    console.log(pages.item(0));
-    console.log(pages[0]);
-    console.log(pages.namedItem('page-0'))
-    console.log(pages['page-2']);
+    // console.log(pages.item(0));
+    // console.log(pages[0]);
+    // console.log(pages.namedItem('page-0'))
+    // console.log(pages['page-2']);
 }
 
 function nextPage() {
+    if(currentPage + 1 >= pageCount)
+        return;
 
+    pages[currentPage].classList.toggleClass('hidden')
+    
+    currentPage++;
+    
+    pages[currentPage].classList.toggleClass('hidden')
 }
 
 function prevPage() {
