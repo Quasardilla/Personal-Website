1ea41fa5e3a7fd0d29e95e34c1808ea0c5f04e13
Quasar <trottinger25@pascack.org>
2024-03-07 10:03:04 -0500

    Side Bar

--- script/script.js
+++ script/script.js
@@ -6,24 +6,31 @@ initializePages();
 
 function initializePages() {
     pages = document.getElementsByClassName('page');
-    // pages = document.querySelectorAll('.page');
-    // console.log(pages.item(0));
-    // console.log(pages[0]);
-    // console.log(pages.namedItem('page-0'))
-    // console.log(pages['page-2']);
 }
 
 function nextPage() {
     if(currentPage + 1 >= pageCount)
         return;
 
-    pages[currentPage].classList.toggle('hidden')
+    pages[currentPage].classList.toggle('hidden');
     
     currentPage++;
     
-    pages[currentPage].classList.toggle('hidden')
+    pages[currentPage].classList.toggle('hidden');
 }
 
 function prevPage() {
+    if(currentPage - 1 < 0)
+        return;
 
+    pages[currentPage].classList.toggle('hidden');
+    
+    currentPage--;
+    
+    pages[currentPage].classList.toggle('hidden');
+}
+
+function toggleNav() {
+    let navbar = document.getElementById('sidebar-nav');
+    navbar.classList.toggle('hidden');
 }
